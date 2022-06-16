cf20b270af0e6c8997eeadea5a4b7dfcbd2e2449
Luis <luis.becerra.15.12@gmail.com>
2022-06-15 23:31:23 -0300

    Api secret

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const url = "https://sheets.googleapis.com/v4/spreadsheets/1YSI89ygeY9W5yag-dX8p4IcuB87rV1S8PGiUz4L6abk/values/Datos_Personales!A:B?key=AIzaSyA9PPOcYQLIR5clBtIpdTH9t94yie8iwF4";
+import {id} from './id.js';
+
+//TODO: handle it with gitattribute or limitate access.
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${id}/values/Datos_Personales!A:B?key=AIzaSyA9PPOcYQLIR5clBtIpdTH9t94yie8iwF4`;
 
 // Get the data from the Google Sheet
 fetch(url)
